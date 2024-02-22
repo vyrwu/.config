e32fd0c84f77e4daa6d563134a8bf65f28a09247
Aleksander Nowak <aleksander.nowak@zeronorth.com>
2024-02-22 12:48:24 +0100

    feat(SELF): Add LSP diagnostics help

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,9 +88,11 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
     end,
     opts = {
       defaults = {
+        wrap_results = true,
         vimgrep_arguments = {
           "rg",
           "-L",
@@ -116,6 +118,9 @@ require("lazy").setup({
         pickers = {
           find_files = {
             hidden = true,
+          },
+          diagnostics = {
+            line_width = "full",
           },
         },
       },
@@ -281,6 +286,42 @@ require("lazy").setup({
           "hrsh7th/cmp-buffer",
           "hrsh7th/cmp-path",
         },
+        init = function()
+          local opts = {} -- Global mappings.
+          vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+          vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set(
+            "n",
+            "<space>wa",
+            vim.lsp.buf.add_workspace_folder,
+            opts
+          )
+          vim.keymap.set(
+            "n",
+            "<space>wr",
+            vim.lsp.buf.remove_workspace_folder,
+            opts
+          )
+          vim.keymap.set("n", "<space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set(
+            { "n", "v" },
+            "<space>ca",
+            vim.lsp.buf.code_action,
+            opts
+          )
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        end,
         config = function()
           local cmp = require("cmp")
           cmp.setup({
