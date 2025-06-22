91696da96c57a0d09a3bb41af54e106e5afeeec8
vyrwu <dev.anowak@gmail.com>
2025-06-22 22:18:53 +0200

    feat(nix): increase download buffer

--- nix/modules/nix.nix
+++ nix/modules/nix.nix
@@ -5,6 +5,10 @@
 
   nix.settings.experimental-features = "nix-command flakes";
 
+  # Increase download buffer to overcome warnings:
+  # ref: https://github.com/NixOS/nix/issues/11728
+  nix.settings.download-buffer-size = 524288000;
+
   nix.optimise.automatic = true;
 
   nixpkgs = {
